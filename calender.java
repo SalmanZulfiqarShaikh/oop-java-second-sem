ba33de1c3e76f8868c09a2cb2e5df463c525b088
Salman Zulfiqar Shaikh <salmanzulfiqar04@gmail.com>
2025-11-12 04:10:58 +0500

    calender done!

--- calender.java
+++ calender.java
@@ -1,33 +1,37 @@
-import java.util.Scanner;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-public class calender {
-    
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        
+public class calender {
+
+    public static void main (String[] args) {
+
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please Enter Year");
+        System.out.println("Please Enter the Year!");
         int year = input.nextInt();
-        System.out.println("Please Enter Month (1-12)");
+
+        System.out.println("Please Enter the Month!");
         int month = input.nextInt();
 
+        LocalDate firstDayofMonth = LocalDate.of(year,month,1);
+        int DaysinMonth = firstDayofMonth.lengthOfMonth();
+        DayOfWeek firstDayOfWeek = firstDayofMonth.getDayOfWeek();
+        int valueOfWeek = firstDayOfWeek.getValue();
 
-        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
-        DayOfWeek dayOfWeek = firstDayOfMonth.getDayOfWeek();
-        int value = dayOfWeek.getValue(); // 1=Monday, 7=Sunday
-        int daysInMonth = firstDayOfMonth.lengthOfMonth();
         System.out.println("Mon Tue Wed Thu Fri Sat Sun");
-        for (int i = 1; i < value; i++) {
-            System.out.print("   ");
+
+        for (int i = 1; i < valueOfWeek; i++) {
+            System.out.print("    ");
         }
-        for (int day = 1; day <= daysInMonth; day++) {
-            System.out.printf("%3d ", day);
-            if ((day + value - 1) % 7 == 0) {
+
+        for (int i = 1; i <= DaysinMonth; i++) {
+            System.out.printf("%3d",i);
+            if ((i + valueOfWeek - 1) %7 == 0) {
                 System.out.println();
             }
         }
+
         input.close();
     }
 }
